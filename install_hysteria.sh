37177e692839956fb8677727ee2de4593ca9de6a
China-numberone <108204473+China-numberone@users.noreply.github.com>
2025-08-05 10:48:45 +0800

    install_hysteria.sh

--- install_hysteria.sh
+++ install_hysteria.sh
@@ -137,7 +137,8 @@ IPV6=$(ip -6 addr show scope global | grep inet6 | head -n1 | awk '{print $2}' |
 IPV4=$(ip -4 addr show scope global | grep inet | head -n1 | awk '{print $2}' | cut -d'/' -f1)
 
 # 提取端口号
-PORT=$(grep '^listen:' /etc/hysteria/config.yaml | sed -E 's/.*\]:([0-9]+).*$/\1/')
+PORT=$(grep '^listen:' /etc/hysteria/config.yaml | grep -oE '[0-9]+$')
+
 
 # 提取认证密码
 # PASSWORD=$(awk '/^auth:/,/^$/{if($1=="userpass:"){print $2}}' /etc/hysteria/config.yaml)
